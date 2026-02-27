b25ad085c687c1a6fd573da010ba7874557cc657
28debbyz <28debbyz@students.harker.org>
2026-02-26 16:34:29 -0800

    fixed rotation

--- src/main/java/frc/robot/AlignConstants.java
+++ src/main/java/frc/robot/AlignConstants.java
@@ -8,10 +8,9 @@ public class AlignConstants {
     //public static final Translation2d HUB_RED = new Translation2d(11.979, 4.044);
 
 
-    public static final Pose2d CLIMB_CENTER = new Pose2d(1.507, 3.745, new Rotation2d(-Math.PI)); 
+    public static final Pose2d CLIMB_CENTER = new Pose2d(1.507, 3.745, new Rotation2d(0)); 
     //public static final Pose2d CLIMB_RIGHT = new Pose2d(0, 0, new Rotation2d(0));
 
     public static final Transform2d RIGHT_OFFSET = new Transform2d(0, -0.1524, new Rotation2d(0)); 
     public static final Transform2d LEFT_OFFSET = new Transform2d(0, 0.1524, new Rotation2d(0));
 }
-
